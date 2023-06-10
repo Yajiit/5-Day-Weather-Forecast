5b92920fcabd901a97599b2da6a84d7854066b09
Yajiit <tjagerter@gmail.com>
2023-06-10 18:33:25 -0400

    refetches most recently searched city on page load. changed hourly forecast to display by day. placed current day's forecast on top of the others. added color CSS styling to high and low temp.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,9 @@ searchForm.addEventListener('submit', function(event) {
   if (initialSearchHistory) {
     const parsedSearchHistory = JSON.parse(initialSearchHistory);
     displaySearchHistory(parsedSearchHistory);
+    const mostRecentCity = parsedSearchHistory[0];
+    // display most recently searched city on page load
+  fetchForecast(mostRecentCity);
   }
 
 
@@ -79,7 +82,7 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid
     // parses the response w JSON and stores as "data"
     const data = await response.json();
     // puts parsed data into displayForecast function
-    displayForecast(data);
+    displayForecast(city, data);
   } catch (error) {
     // log for errors
     console.log('Error:', error);
@@ -89,13 +92,22 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid
 
 
 // Function to display the weather forecast to the div container
-function displayForecast(data) {
+function displayForecast(city, data) {
 
   // clears any existing content of that div container
   forecastContainer.innerHTML = '';
+  const currentForecast = {
+    date: data.list[0].dt_txt,
+    temperature: data.list[0].main.temp,
+    tempLow: data.list[0].main.temp_min,
+    tempHigh: data.list[0].main.temp_max,
+    description: data.list[0].weather[0].description,
+    icon: data.list[0].weather[0].icon
+  };
 
-  // Extract data for each forecast entry and stors as "forecasts"
-  const forecasts = data.list.map(item => {
+  const nextFiveDays = data.list.slice(1, 6).map(item => {
+
+  // Extract data for each forecast entry 
     return {
       date: item.dt_txt,
       temperature: item.main.temp,
@@ -107,117 +119,72 @@ function displayForecast(data) {
     };
   });
 
-  let row;
-  // Create forecast cards and append them to the container
-  forecasts.forEach((forecast, i) => {
-    if (i % 4 === 0){
-      row = document.createElement('div');
-      row.classList.add('forecast-row');
-    }
+  const forecastsByDay = groupForecastsByDay(data.list);
+  const currentDayForecast = forecastsByDay[0];
+  const dailyForecasts = forecastsByDay.slice(1);
+  const currentForecastCard = document.createElement('div');
+  currentForecastCard.classList.add('forecast-current');
+  currentForecastCard.innerHTML = `
+    <h2>${city}</h3>
+    <h3>${currentDayForecast.date}</h3>
+    <p>Temperature: ${convertKelvinToFahrenheit(currentDayForecast.temperature)} </p>
+    <p id="high">High: ${convertKelvinToFahrenheit(currentDayForecast.tempHigh)} </p>
+    <p id="low">Low: ${convertKelvinToFahrenheit(currentDayForecast.tempLow)} </p>
+    <p>Description: ${currentDayForecast.description}</p>
+    <img src="https://openweathermap.org/img/w/${currentDayForecast.icon}.png" alt="${currentDayForecast.description}">
+  `;
 
-    // used forEach method to iterate over each 'forecast' in the array 'forecasts' [with an s]
-    const card = document.createElement('div');
-    // created 'card' element as a <div>
-    card.classList.add('forecast-card');
-    // added CSS styling class 'forecast-card' to each 'card' element
-    card.innerHTML = `
+  forecastContainer.appendChild(currentForecastCard);
+
+  dailyForecasts.forEach(forecast => {
+    const forecastCard = document.createElement('div');
+    forecastCard.classList.add('forecast-card');
+    forecastCard.innerHTML = `
       <h3>${forecast.date}</h3>
-      <p>Temperature: ${forecast.temperature} </p>
-      <p>Low: ${forecast.tempLow} </p>
-      <p>High: ${forecast.tempHigh} </p>
+      <p>Temperature: ${convertKelvinToFahrenheit(forecast.temperature)} </p>
+      <p id="high">High: ${convertKelvinToFahrenheit(forecast.tempHigh)} </p>
+      <p id="low">Low: ${convertKelvinToFahrenheit(forecast.tempLow)} </p>
       <p>Description: ${forecast.description}</p>
       <img src="https://openweathermap.org/img/w/${forecast.icon}.png" alt="${forecast.description}">
     `;
     // added the HTML to each 'card' with <h3> for time and a <p> for main temp, low temp, high temp, weath description, and icon each.
-    row.appendChild(card);
-    // appended the card to forecastContainer in the HTML document
+    forecastContainer.appendChild(forecastCard);
+  });
 
-    if ((i + 1) % 4 === 0 || i === forecasts.length - 1) {
-      forecastContainer.appendChild(row);
+}
+// Function to group forecasts by day
+function groupForecastsByDay(forecasts) {
+  const forecastsByDay = {};
+  forecasts.forEach(forecast => {
+    const date = forecast.dt_txt.split(' ')[0]; // Extract the date part only
+    if (!forecastsByDay[date]) {
+      // if there's no forecast with the same date: pull the entire forecast
+      forecastsByDay[date] = {
+        date: date,
+        temperature: forecast.main.temp,
+        tempLow: forecast.main.temp_min,
+        tempHigh: forecast.main.temp_max,
+        description: forecast.weather[0].description,
+        icon: forecast.weather[0].icon
+      };
+    } else {
+      // if there is an existing forecast on that date, check if the high temperature is higher and display the highest
+      const existingForecast = forecastsByDay[date];
+      if (forecast.main.temp > existingForecast.tempHigh) {
+        existingForecast.tempHigh = forecast.main.temp;
+      }
+      // if there is an existing forecast on that date, check if the low temperature is lower and display the lowest
+      if (forecast.main.temp_min < existingForecast.tempLow) {
+        existingForecast.tempLow = forecast.main.temp_min;
+      }
     }
   });
+  return Object.values(forecastsByDay);
 }
 
-// window.onload = function() {
-//   // Event listener for form submission
-//   searchForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const cityInput = document.getElementById('city-input');
-//     const city = cityInput.value;
-
-//     // Call the function to fetch weather forecast for the entered city
-//     fetchForecast(city);
-
-//     // Add the searched city to local storage
-//     addCityToLocalStorage(city);
-
-//     // Clear the input field after the search
-//     cityInput.value = '';
-//   });
-
-//   // Function to fetch the forecast data
-//   async function fetchForecast(city) {
-//     // Weather forecast API URL
-//     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-//     try {
-//       // Fetches the forecast for the chosen city and stores it as "response"
-//       const response = await fetch(apiUrl);
-//       // Parses the response as JSON and stores it as "data"
-//       const data = await response.json();
-//       // Puts the parsed data into the displayForecast function
-//       displayForecast(data);
-//     } catch (error) {
-//       // Log any errors
-//       console.log('Error:', error);
-//     }
-//   }
-
-//   // Function to add the searched city to local storage
-//   function addCityToLocalStorage(city) {
-//     let searchHistory = localStorage.getItem('searchHistory');
-//     if (searchHistory) {
-//       searchHistory = JSON.parse(searchHistory);
-//       // Remove the city if it already exists in the search history
-//       searchHistory = searchHistory.filter(item => item !== city);
-//       // Add the new city to the beginning of the search history array
-//       searchHistory.unshift(city);
-//       // Keep only the last 5 searches
-//       searchHistory = searchHistory.slice(0, 5);
-//     } else {
-//       searchHistory = [city];
-//     }
-
-//     // Save the updated search history to local storage
-//     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-//     // Display the search history as buttons
-//     displaySearchHistory(searchHistory);
-//   }
-
-//   // Function to display the search history as buttons
-//   function displaySearchHistory(searchHistory) {
-//     searchHistoryContainer.innerHTML = '';
-
-//     searchHistory.forEach(city => {
-//       const button = document.createElement('button');
-//       button.textContent = city;
-//       button.classList.add('btn', 'btn-secondary', 'mb-2', 'd-block');
-//       button.addEventListener('click', function() {
-//         fetchForecast(city);
-//       });
-
-//       searchHistoryContainer.appendChild(button);
-//     });
-//   }
-
-//   // Load and display the search history on page load
-//   const initialSearchHistory = localStorage.getItem('searchHistory');
-//   if (initialSearchHistory) {
-//     const parsedSearchHistory = JSON.parse(initialSearchHistory);
-//     displaySearchHistory(parsedSearchHistory);
-
-//     // Fetch the weather forecast for the most recently searched city
-//     const mostRecentCity = parsedSearchHistory[0];
-//     fetchForecast(mostRecentCity);
-//   }
-// };
+// function to convert temperature from Kelvin to Fahrenheit
+function convertKelvinToFahrenheit(temperature) {
+  const fahrenheit = (temperature - 273.15) * 9/5 + 32;
+  // cut off at tenth decimal
+  return fahrenheit.toFixed(1);
+}
